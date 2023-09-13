83fbdb9344b10ffb8a36b9440602d78820d9e1c6
전병길 <jeonbg@kakao.com>
2023-09-13 09:54:04 +0900

    fix: 사이드 헤더 > 깃헙 링크 주석처리

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,8 +1,4 @@
-import Link from "next/link"
-
 import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
-import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
 
@@ -14,7 +10,7 @@ export function SiteHeader() {
 
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
-            <Link
+            {/* <Link
               href={siteConfig.links.github}
               target="_blank"
               rel="noreferrer"
@@ -29,7 +25,7 @@ export function SiteHeader() {
 
                 <span className="sr-only">GitHub</span>
               </div>
-            </Link>
+            </Link> */}
 
             <ThemeToggle />
           </nav>
