9b1afbc12c12bb2cfdbba08825c84af2b8f2cde7
전병길 <jeonbg@kakao.com>
2023-09-17 13:03:45 +0900

    fix(monster): 몬스터 검색인풋 추가, Framer-Motion 테스트 로직 추가 (#13)

--- components/common/monster-image.tsx
+++ components/common/monster-image.tsx
@@ -8,6 +8,7 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useQueryClient } from "@tanstack/react-query"
 import axios from "axios"
 import clsx from "clsx"
+import { motion } from "framer-motion"
 import { useForm } from "react-hook-form"
 
 import { Button } from "../ui/button"
@@ -15,6 +16,7 @@ import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form"
 import { Input } from "../ui/input"
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover"
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group"
+import { useToast } from "../ui/use-toast"
 
 interface MonsterImageProps extends HTMLAttributes<HTMLSpanElement> {
   monsterInfo: MonsterInfo
@@ -25,12 +27,14 @@ export default function MonsterImage({
   className,
   ...rest
 }: MonsterImageProps) {
+  const { toast } = useToast()
+
+  const queryClient = useQueryClient()
+
   const form = useForm<MonsterInfo>({
     resolver: zodResolver(monsterInfoSchema),
     defaultValues: monsterInfo,
   })
-
-  const queryClient = useQueryClient()
 
   const originName = form.watch("originName")
 
@@ -40,170 +44,173 @@ export default function MonsterImage({
     await axios
       .patch("/api/monster", monsterInfo)
       .then(() => queryClient.invalidateQueries({ queryKey: ["monster"] }))
+      .then(() => toast({ title: "저장되었습니다.", duration: 1000 }))
   }
 
   return (
-    <Popover modal>
-      <PopoverTrigger asChild>
-        <span
-          className={clsx(
-            "sprite relative inline-block rounded-xl",
-            className,
-            originName
-          )}
-          {...rest}
-        >
-          <i
+    <motion.div layout>
+      <Popover modal>
+        <PopoverTrigger asChild>
+          <span
             className={clsx(
-              "monster-elements absolute right-[-4px] top-[-2px] inline-block",
-              elementType
+              "sprite relative inline-block rounded-xl",
+              className,
+              originName
             )}
-          />
-        </span>
-      </PopoverTrigger>
-
-      <PopoverContent>
-        <div className="space-y-2">
-          <h2 className="text-sm text-muted-foreground">몬스터 정보 변경</h2>
-        </div>
-
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(handleSubmit)}
-            className="space-y-4"
+            {...rest}
           >
-            <FormField
-              control={form.control}
-              name="elementType"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>타입</FormLabel>
-
-                  <FormControl>
-                    <RadioGroup
-                      className="flex space-x-2"
-                      onValueChange={field.onChange}
-                      defaultValue={field.value || ""}
-                    >
-                      <FormItem className="flex items-center space-x-[4px] space-y-0">
-                        <FormControl>
-                          <RadioGroupItem value="fire" />
-                        </FormControl>
-                        <FormLabel>불</FormLabel>
-                      </FormItem>
-
-                      <FormItem className="flex items-center space-x-[4px] space-y-0">
-                        <FormControl>
-                          <RadioGroupItem value="water" />
-                        </FormControl>
-                        <FormLabel>물</FormLabel>
-                      </FormItem>
-
-                      <FormItem className="flex items-center space-x-[4px] space-y-0">
-                        <FormControl>
-                          <RadioGroupItem value="wind" />
-                        </FormControl>
-                        <FormLabel>풍</FormLabel>
-                      </FormItem>
-
-                      <FormItem className="flex items-center space-x-[4px] space-y-0">
-                        <FormControl>
-                          <RadioGroupItem value="light" />
-                        </FormControl>
-                        <FormLabel>빛</FormLabel>
-                      </FormItem>
-
-                      <FormItem className="flex items-center space-x-[4px] space-y-0">
-                        <FormControl>
-                          <RadioGroupItem value="dark" />
-                        </FormControl>
-                        <FormLabel>암</FormLabel>
-                      </FormItem>
-                    </RadioGroup>
-                  </FormControl>
-                </FormItem>
+            <i
+              className={clsx(
+                "monster-elements absolute right-[-4px] top-[-2px] inline-block",
+                elementType
               )}
             />
+          </span>
+        </PopoverTrigger>
 
-            <FormField
-              control={form.control}
-              name="originName"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>원본 이름</FormLabel>
+        <PopoverContent>
+          <div className="space-y-2">
+            <h2 className="text-sm text-muted-foreground">몬스터 정보 변경</h2>
+          </div>
 
-                  <FormControl>
-                    <Input
-                      {...field}
-                      disabled
-                      readOnly
-                      value={field.value || ""}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(handleSubmit)}
+              className="space-y-4"
+            >
+              <FormField
+                control={form.control}
+                name="elementType"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>타입</FormLabel>
 
-            <FormField
-              control={form.control}
-              name="monsterName"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>몬스터 이름</FormLabel>
+                    <FormControl>
+                      <RadioGroup
+                        className="flex space-x-2"
+                        onValueChange={field.onChange}
+                        defaultValue={field.value || ""}
+                      >
+                        <FormItem className="flex items-center space-x-[4px] space-y-0">
+                          <FormControl>
+                            <RadioGroupItem value="fire" />
+                          </FormControl>
+                          <FormLabel>불</FormLabel>
+                        </FormItem>
 
-                  <FormControl>
-                    <Input
-                      {...field}
-                      onChange={(e) => {
-                        const space = /\s/
-                        if (space.test(e.target.value)) {
-                          return
-                        }
+                        <FormItem className="flex items-center space-x-[4px] space-y-0">
+                          <FormControl>
+                            <RadioGroupItem value="water" />
+                          </FormControl>
+                          <FormLabel>물</FormLabel>
+                        </FormItem>
 
-                        field.onChange(e)
-                      }}
-                      value={field.value || ""}
-                      placeholder="전속성 공통이름 (ex. 드루이드)"
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
+                        <FormItem className="flex items-center space-x-[4px] space-y-0">
+                          <FormControl>
+                            <RadioGroupItem value="wind" />
+                          </FormControl>
+                          <FormLabel>풍</FormLabel>
+                        </FormItem>
 
-            <FormField
-              control={form.control}
-              name="keyword"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>키워드</FormLabel>
+                        <FormItem className="flex items-center space-x-[4px] space-y-0">
+                          <FormControl>
+                            <RadioGroupItem value="light" />
+                          </FormControl>
+                          <FormLabel>빛</FormLabel>
+                        </FormItem>
 
-                  <FormControl>
-                    <Input
-                      {...field}
-                      placeholder="(ex. 물,물드루)"
-                      onChange={(e) => {
-                        const replaceValue = e.target.value.split(",")
-                        const space = /\s/
+                        <FormItem className="flex items-center space-x-[4px] space-y-0">
+                          <FormControl>
+                            <RadioGroupItem value="dark" />
+                          </FormControl>
+                          <FormLabel>암</FormLabel>
+                        </FormItem>
+                      </RadioGroup>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
 
-                        if (space.test(e.target.value)) {
-                          return
-                        }
+              <FormField
+                control={form.control}
+                name="originName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>원본 이름</FormLabel>
 
-                        field.onChange(replaceValue)
-                      }}
-                      value={field.value || ""}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
+                    <FormControl>
+                      <Input
+                        {...field}
+                        disabled
+                        readOnly
+                        value={field.value || ""}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
 
-            <Button className="w-full" type="submit">
-              수정
-            </Button>
-          </form>
-        </Form>
-      </PopoverContent>
-    </Popover>
+              <FormField
+                control={form.control}
+                name="monsterName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>몬스터 이름</FormLabel>
+
+                    <FormControl>
+                      <Input
+                        {...field}
+                        onChange={(e) => {
+                          const space = /\s/
+                          if (space.test(e.target.value)) {
+                            return
+                          }
+
+                          field.onChange(e)
+                        }}
+                        value={field.value || ""}
+                        placeholder="전속성 공통이름 (ex. 드루이드)"
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="keyword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>키워드</FormLabel>
+
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="(ex. 물,물드루)"
+                        onChange={(e) => {
+                          const replaceValue = e.target.value.split(",")
+                          const space = /\s/
+
+                          if (space.test(e.target.value)) {
+                            return
+                          }
+
+                          field.onChange(replaceValue)
+                        }}
+                        value={field.value || ""}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <Button className="w-full" type="submit">
+                수정
+              </Button>
+            </form>
+          </Form>
+        </PopoverContent>
+      </Popover>
+    </motion.div>
   )
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+/** 디바운스 함수 */
+export function debounce<F extends (...args: Parameters<F>) => ReturnType<F>>(
+  func: F,
+  waitFor: number = 500
+): (...args: Parameters<F>) => void {
+  let timeout: NodeJS.Timeout
+
+  return (...args: Parameters<F>): void => {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => func(...args), waitFor)
+  }
+}
