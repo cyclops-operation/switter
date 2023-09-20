9faf8654618beeb533f8dc7097787bea95b17eaf
p-acid <qkrtkstjd01@gmail.com>
2023-09-20 14:10:45 +0900

    style: 다크 모드 반영 및 AccountLayout 수정

--- app/(public)/(account)/src/ui/header.tsx
+++ app/(public)/(account)/src/ui/header.tsx
@@ -6,11 +6,11 @@ export interface AccountHeaderProps {
 const AccountHeader = ({ title, description }: AccountHeaderProps) => {
   return (
     <div className="flex flex-col gap-2">
-      <h1 className="text-5xl font-bold text-zinc-600 dark:text-zinc-300">
+      <h1 className="break-keep text-5xl font-bold text-zinc-600 dark:text-zinc-300">
         {title}
       </h1>
       {description ? (
-        <p className="whitespace-pre-line text-lg text-zinc-400 dark:text-zinc-500">
+        <p className="whitespace-pre-line break-keep text-lg text-zinc-400 dark:text-zinc-500">
           {description}
         </p>
       ) : null}
