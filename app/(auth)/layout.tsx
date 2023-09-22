96deee3147778dc2052efc1c70bc610fad7091f9
p-acid <qkrtkstjd01@gmail.com>
2023-09-22 12:33:58 +0900

    fix: (auth) 세그먼트 내 리다이렉트 로직 추가

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,26 @@
+import { redirect } from "next/navigation"
+
+import { accountStatus } from "@/interface/account"
+
+import { pageRoute } from "@/lib/page-route"
 import { SiteHeader } from "@/components/layout/site-header"
+
+import { getSessionAccount } from "../api/account/action"
 
 interface AuthLayoutProps {
   children: React.ReactNode
 }
 
-const AuthLayout = ({ children }: AuthLayoutProps) => {
+const AuthLayout = async ({ children }: AuthLayoutProps) => {
+  const account = await getSessionAccount()
+
+  const isNotAuthorized =
+    account === null || account?.status === accountStatus.Enum.PENDING
+
+  if (isNotAuthorized) {
+    redirect(pageRoute.SignIn)
+  }
+
   return (
     <div className="relative flex h-full flex-col">
       <SiteHeader />
