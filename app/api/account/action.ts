e1a2d33e817d83cedc82a8e114dc32f330479a0e
p-acid <qkrtkstjd01@gmail.com>
2023-09-22 11:54:10 +0900

    docs: getSessionAccount 주석 추가

--- app/api/account/action.ts
+++ app/api/account/action.ts
@@ -4,6 +4,9 @@ import prisma from "@/lib/prisma"
 
 import { authOptions } from "../auth/[...nextauth]/route"
 
+/**
+ * 로그인 한 유저(현재 세션에 저장되어 있는)의 데이터를 가져옵니다.
+ */
 const getSessionAccount = async () => {
   const session = await getServerSession(authOptions)
 
