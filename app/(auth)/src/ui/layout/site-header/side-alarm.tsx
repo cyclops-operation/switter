56cbefc493d1929f258d3a7593d94f6bd8b23259
p-acid <qkrtkstjd01@gmail.com>
2023-10-02 16:58:04 +0900

    style: 알림 목록 Sheet 컴포넌트로 변경

--- app/(auth)/src/ui/layout/site-header/side-alarm.tsx
+++ app/(auth)/src/ui/layout/site-header/side-alarm.tsx
@@ -6,20 +6,19 @@ import Link from "next/link"
 import { Alarm } from "@/interface/alarm"
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import axios from "axios"
-import { AnimatePresence, motion } from "framer-motion"
 
 import { apiRoute } from "@/lib/api-route"
-import { Button } from "@/components/ui/button"
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 import { Icons } from "@/components/common/icons"
 
 import Loading from "../../../../../../components/common/loading"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "../../../../../../components/ui/card"
 
 const SideAlarm = () => {
   const [isAlarmOpen, setIsAlarmOpen] = useState(false)
@@ -31,6 +30,8 @@ const SideAlarm = () => {
     async () =>
       await axios.get<Alarm[]>(apiRoute.Alarm).then(({ data }) => data)
   )
+
+  const isEmpty = alarms?.length === 0
 
   const { mutate: patchAlarmReadM } = useMutation(
     async (id: number) =>
@@ -44,57 +45,58 @@ const SideAlarm = () => {
     }
   )
 
+  const changeAlarmOpenState = () => {
+    setIsAlarmOpen((prev) => !prev)
+  }
+
+  const clickAlarm = (isRead: boolean, id: number) => {
+    if (!isRead) {
+      patchAlarmReadM(id)
+    }
+    changeAlarmOpenState()
+  }
+
   const hasUnreadAlarm = !!alarms?.find(({ isRead }) => !isRead)
 
   return (
-    <>
-      <Button variant="ghost" onClick={() => setIsAlarmOpen((prev) => !prev)}>
+    <Sheet modal open={isAlarmOpen} onOpenChange={changeAlarmOpenState}>
+      <SheetTrigger className="cursor-pointer" asChild>
         {hasUnreadAlarm ? <Icons.bellRing /> : <Icons.bell />}
-      </Button>
-      <AnimatePresence>
-        {isAlarmOpen ? (
-          <motion.div
-            className="fixed right-3 top-20"
-            initial={{ y: -5, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            exit={{ y: -5, opacity: 0 }}
-          >
-            <Card className="z-[100000] h-[50vh] w-96 overflow-y-scroll">
-              <CardHeader>
-                <CardTitle>알림 목록</CardTitle>
-                <CardDescription>
-                  전달된 알림 목록을 확인해보세요
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="h-full space-y-2">
-                {isLoading ? (
-                  <Loading />
-                ) : (
-                  alarms?.map(({ id, title, description, isRead, url }) => (
-                    <Link
-                      key={id}
-                      className="flex justify-between gap-2 transition-transform hover:translate-y-[1px]"
-                      href={url}
-                      onClick={() => !isRead && patchAlarmReadM(id)}
-                    >
-                      <div className="space-y-1">
-                        <p className="text-sm">{title}</p>
-                        {description ? (
-                          <p className="text-xs text-zinc-400">{description}</p>
-                        ) : null}
-                      </div>
-                      {!isRead ? (
-                        <div className="mt-2 h-[6px] w-[6px] rounded-full bg-green-600" />
-                      ) : null}
-                    </Link>
-                  ))
-                )}
-              </CardContent>
-            </Card>
-          </motion.div>
-        ) : null}
-      </AnimatePresence>
-    </>
+      </SheetTrigger>
+
+      <SheetContent className="overflow-y-scroll" side="right">
+        <SheetHeader>
+          <SheetTitle>알림 목록</SheetTitle>
+          <SheetDescription>전달된 알림 목록을 확인해보세요</SheetDescription>
+        </SheetHeader>
+        {isLoading ? (
+          <Loading />
+        ) : isEmpty ? (
+          <div>알림이 없습니다.</div>
+        ) : (
+          <div className="flex flex-col gap-2 pt-8">
+            {alarms?.map(({ id, title, description, isRead, url }) => (
+              <Link
+                key={id}
+                className="flex justify-between gap-2 transition-transform hover:translate-y-[1px]"
+                href={url}
+                onClick={() => clickAlarm(isRead, id)}
+              >
+                <div className="space-y-1">
+                  <p className="text-sm">{title}</p>
+                  {description ? (
+                    <p className="text-xs text-zinc-400">{description}</p>
+                  ) : null}
+                </div>
+                {!isRead ? (
+                  <div className="mt-2 h-[6px] w-[6px] rounded-full bg-green-600" />
+                ) : null}
+              </Link>
+            ))}
+          </div>
+        )}
+      </SheetContent>
+    </Sheet>
   )
 }
 
