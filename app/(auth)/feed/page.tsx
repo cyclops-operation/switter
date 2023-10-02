841dd99377e6f818dbb2783d08a2c41cec2c98b7
Genie <jeonbg@kakao.com>
2023-10-02 15:38:34 +0900

    fix(feed): layout 스타일 수정

--- app/(auth)/feed/page.tsx
+++ app/(auth)/feed/page.tsx
@@ -2,7 +2,7 @@ import FeedList from "../src/ui/list/feed-list"
 
 export default function FeedPage() {
   return (
-    <section className="h-full">
+    <section className="container h-full">
       <FeedList />
     </section>
   )
