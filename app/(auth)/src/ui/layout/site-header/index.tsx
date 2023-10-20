28f46683b216dc57844a124ca84384319a2c7251
p-acid <qkrtkstjd01@gmail.com>
2023-10-20 22:46:54 +0900

    fix: 빌드 에러 수정

--- app/(auth)/src/ui/layout/site-header/index.tsx
+++ app/(auth)/src/ui/layout/site-header/index.tsx
@@ -8,6 +8,7 @@ export default function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+        {/* @ts-expect-error Async Server Component */}
         <MainNav />
 
         <div className="flex flex-1 items-center justify-end space-x-4">
