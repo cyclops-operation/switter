293fc7a6f367fdeef5ccf816bc367f87b0186c34
전병길 <jeonbg@kakao.com>
2023-10-23 21:29:06 +0900

    feat(navigation): navigation 반응형 스타일 추가 및 어드민 페이지 반응형 스타일 수정 (#82)
    Release-As: 1.0.7
    fixed: #75

--- app/(auth)/src/ui/layout/site-header/sign-out-button.tsx
+++ app/(auth)/src/ui/layout/site-header/sign-out-button.tsx
@@ -2,16 +2,16 @@
 
 import { signOut } from "next-auth/react"
 
-import { Button } from "@/components/ui/button"
+import { Button, ButtonProps } from "@/components/ui/button"
 
-export default function SignOutButton() {
+export default function SignOutButton(props: ButtonProps) {
   const clickSignOut = () => {
     signOut()
   }
 
   return (
-    <Button variant="ghost" onClick={clickSignOut}>
-      Logout
+    <Button variant="ghost" {...props} onClick={clickSignOut}>
+      로그아웃
     </Button>
   )
 }
