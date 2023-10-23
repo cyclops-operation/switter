e749d9608d02eb90935688128a8f335aff358184
p-acid <qkrtkstjd01@gmail.com>
2023-10-23 11:58:52 +0900

    refactor(account): account 세그먼트 반응형 반영

--- app/(account)/layout.tsx
+++ app/(account)/layout.tsx
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation"
 import { userStatus } from "@/interface/user"
 
 import { pageRoute } from "@/lib/page-route"
-import { getServerAccount } from "@/lib/utils"
+import { cn, getServerAccount } from "@/lib/utils"
 import { Card } from "@/components/ui/card"
 
 import accountImage from "/public/images/account-image.jpg"
@@ -24,11 +24,19 @@ export default async function AccountLayout({ children }: AccountLayoutProps) {
 
   return (
     <main className="flex h-screen items-center justify-center bg-zinc-100 dark:bg-zinc-950">
-      <Card className="mx-10 my-52 flex h-[598px] overflow-hidden">
-        <div className="flex w-[400px] flex-col">{children}</div>
+      <Card
+        className={cn(
+          "mx-10 my-52 flex h-[598px] overflow-hidden",
+          "max-mobile:m-0 max-mobile:h-full max-mobile:border-none max-mobile:px-3 max-mobile:py-6", // 레이아웃 관련
+          "max-mobile:bg-[url('/images/account-image.jpg')] max-mobile:bg-cover max-mobile:bg-blend-soft-light" // 백그라운드 이미지 관련
+        )}
+      >
+        <div className="flex w-[400px] flex-col justify-between">
+          {children}
+        </div>
 
         <Image
-          className="object-cover grayscale"
+          className="object-cover grayscale max-mobile:hidden"
           src={accountImage.src}
           alt="account-image"
           width={400}
