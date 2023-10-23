951b5cc9ccd78eae4d3dbda29258b32f56233476
Samuel Park <87939521+p-acid@users.noreply.github.com>
2023-10-23 12:45:46 +0900

    refactor(account): `(account)` 세그먼트 반응형 반영 (#78)

--- app/(account)/src/ui/header.tsx
+++ app/(account)/src/ui/header.tsx
@@ -10,7 +10,7 @@ export default function AccountHeader({
   description,
 }: AccountHeaderProps) {
   return (
-    <CardHeader>
+    <CardHeader className="pb-0">
       <CardTitle>{title}</CardTitle>
 
       {description ? (
